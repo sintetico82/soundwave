17ddebbfe706e7e342b3b2cbbaee385f57083ba9
sintetico82 <sintetico82@gmail.com>
2017-10-23 01:09:10 +0200

    Record wav file

--- soundwave.js
+++ soundwave.js
@@ -4,16 +4,18 @@ var player = require('play-sound')();
 var apiai = require("apiai");
 var fs = require('fs')
 const record = require('node-record-lpcm16');
-let {Detector, Models} = require('snowboy');
+let { Detector, Models } = require('snowboy');
+let eos = require('end-of-stream')
+
+var config = require('./config.json');
 
 //player.play('media/asyoucommand.wav', function(err){
 //    if (err) throw err
 //});
 
-var ACCESS_TOKEN = "cef1797ea3344fba97f4ca376e7ea65b";
 var FILE_BUFFER_PATH = 'command.wav';
 
-var app = apiai(ACCESS_TOKEN, {
+var app = apiai(config.API_AI, {
     language: 'it'
 });
 
@@ -23,79 +25,129 @@ var options = {
 
 
 
-
-
-const models = new Models(); 
+const models = new Models();
 
 models.add({
     file: 'resources/Soundwave.pmdl',
     sensitivity: '0.5',
-    hotwords : 'Soundwave'
+    hotwords: 'Soundwave'
 });
 
-const detector = new Detector({
-    resource: "resources/common.res",
-    models: models,
-    audioGain: 2.0
-});
-
-var t =fs.createWriteStream('command.wav', { encoding: 'binary' })
-
-detector.on('hotword', function (index, hotword,buffer) {
-    if(hotword === 'Soundwave') {
-      console.log("As you command!")
 
 
-     /* let file = fs.createWriteStream('command.wav', { encoding: 'binary' })
-      let mic2 = record.start({
-        threshold: 0.9,
-        silence: '1.0',
-        recordProgram: 'arecord',
+//var t =fs.createWriteStream('command.wav', { encoding: 'binary' })
+
+
+
+function recordCommand() {
+    let file = fs.createWriteStream(FILE_BUFFER_PATH, { encoding: 'binary' });
+
+    let detector = new Detector({
+        resource: "resources/common.res",
+        models: models,
+        audioGain: 2.0
+    });
+
+    let cmd = record.start({
         sampleRateHertz: 16000,
-        verbose: true
-      });
-      mic2.pipe(file);
-      mic2.addListener("end",function(e) {
-        console.log(e);
-      });*/
+        threshold: 0,
+    })
 
-      /*setTimeout(function () {
-        mic2.stop()
-      }, 3000)*/
+    let command_begun = false,
+        time_silence = null,
+        time = new Date();
 
-      var request = app.voiceRequest(options);
-      //var request = app.textRequest('Esci dal programma',options);
-      
-      request.on('response', function(response) {
-          console.log(response);
-      });
-      
-      request.on('error', function(error) {
-          console.log(error);
-      });
-      
-    }
+    detector.on('silence', function () {
+        if (command_begun && time_silence == null) {
+            time_silence = time.getTime()
+            console.log('Silent')
+        }
+        else if (command_begun && time_silence !== null) {
+            if (((new Date()).getTime() - time_silence) > 5000) {
+                record.stop()
+                command_begun = false
+            }
+            else {
+                console.log((new Date()).getTime() + ' minus ' + time_silence)
+            }
+        }
+    })
+    detector.on('sound', function () {
+        if (command_begun) {
+            time_silence = null
+        }
+        else
+            command_begun = true
+        console.log('Noise')
+    })
 
-    console.log('hotword', index, hotword);
-  });
- 
-  detector.on('sound', function (buffer) {
-    // <buffer> contains the last chunk of the audio that triggers the "sound"
-    // event. It could be written to a wav stream.
-    console.log(buffer.length);
-    console.log('sound');
+    cmd.pipe(detector)
+    cmd.pipe(file)
 
-  });
+    eos(cmd, function(err) {
+        if (err) return console.log('stream had an error or closed early');
+        console.log("End of write command");
+        sendCommandToAI();
+
+    });
+
+}
+
+
+function sendCommandToAI() {
+
+    var request = app.voiceRequest(options);
+    
+    request.on('response', function(response) {
+        console.log(response);
+    });
+    
+    request.on('error', function(error) {
+        console.log(error);
+    });
+    
+    fs.readFile(FILE_BUFFER_PATH, function(error, buffer) {
+        if (error) {
+            console.log(error);
+        } else {
+            request.write(buffer);
+        }
+    
+        request.end();
+    });
+}
 
 
 
-const mic = record.start({
-    recordProgram: 'arecord',
-    threshold: 0,
-    verbose: false
-});
+function waitForCommands() {
 
-mic.pipe(detector);
+    let detector = new Detector({
+        resource: "resources/common.res",
+        models: models,
+        audioGain: 2.0
+    });
 
+    detector.on('hotword', function (index, hotword, buffer) {
+        if (hotword === 'Soundwave') {
+
+            console.log("As you command!")
+            recordCommand();
+        }
+
+        console.log('hotword', index, hotword);
+    });
+
+    const mic = record.start({
+        recordProgram: 'arecord',
+        threshold: 0,
+        verbose: false
+    });
+
+    mic.pipe(detector);
+
+}
 
 console.log("** SOUNDWAVE START **");
+
+//waitForCommands();
+sendCommandToAI();
